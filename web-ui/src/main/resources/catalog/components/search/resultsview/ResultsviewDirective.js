dffe5d8381049154aae065dde1dd3cbbacee5612
sebr72 <sebastien_riollet@hotmail.com>
2026-08-26 14:58:47 +0200

    In results view, concatenate the description's lines into a single one.

--- web-ui/src/main/resources/catalog/components/search/resultsview/ResultsviewDirective.js
+++ web-ui/src/main/resources/catalog/components/search/resultsview/ResultsviewDirective.js
@@ -219,17 +219,7 @@
 
           scope.abstractBriefMaker = function (resourceAbstract) {
             if (resourceAbstract) {
-              var abstractParagraphs = resourceAbstract.split("\n");
-              var abstractBrief = "";
-              for (var index = 0; index < abstractParagraphs.length; index++) {
-                var abstractBrief = abstractBrief + abstractParagraphs[index] + "\n";
-                if (abstractBrief.length > 50) {
-                  break;
-                }
-              }
-
-              //remove the last line break character
-              return abstractBrief.substring(0, abstractBrief.length - 1);
+              return resourceAbstract.replaceAll("\n", " ");
             }
           };
 
